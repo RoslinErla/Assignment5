e4b17a7c869f14804fd049d9f7f7fd547102a015
Róslín <roslin19@ru.is>
2019-09-05 15:00:04 +0000

    algorithm written for sequence

--- max_int.py
+++ max_int.py
@@ -3,7 +3,9 @@
 num_int = int(input("Input a number: "))  
 max_int=0
 
+
 while num_int >= 0:
+    #if num_int is bigger or the same as the already biggest number then that number becomes the max_int 
     if num_int >= max_int:
         max_int = num_int
     num_int = int(input("Input a number: "))
---
+++ sequence.py
@@ -0,0 +1,27 @@
+#Design an algorithm that generates the first n numbers 
+# in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+#We know the first three integers so we put them as a start.
+first=1
+second =2
+third =3 
+
+
+for i in range (n+1):
+    if i==0:
+        print(1)
+    if i==1:
+        print(2)
+    if i == 3:
+        print(3)
+    if i > 3:
+        #We know that the sequence works by adding together the three integers before so we calculate that.
+        #Then we update the variables first, second, and third. Now all of them move up. The second integer is now the first
+        #the third integer is now the second, and the fourth integer is now the third. 
+        fourth= first+second+third
+        first=second
+        second=third
+        third=fourth
+        print(fourth)
